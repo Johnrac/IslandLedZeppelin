f49ba387e185346cf9ad12835e54ebeedcbafe9d
Johnrac <75098204+Johnrac@users.noreply.github.com>
2025-06-29 23:51:59 +0300

    ADD println for Printer

--- src/main/java/com/javarush/island/pukhov/output/ConsolePrinter.java
+++ src/main/java/com/javarush/island/pukhov/output/ConsolePrinter.java
@@ -4,9 +4,14 @@ public class ConsolePrinter implements Printer{
     @SuppressWarnings("java:S106")
     @Override
     public void print(Object information) {
+        System.out.print(information);
+    }
+    @SuppressWarnings("java:S106")
+    @Override
+    public void println(Object information) {
         System.out.println(information);
     }
-
+    
     @Override
     public void printError(RuntimeException exception) {
         exception.printStackTrace();
--- src/main/java/com/javarush/island/pukhov/output/Printer.java
+++ src/main/java/com/javarush/island/pukhov/output/Printer.java
@@ -2,5 +2,8 @@ package com.javarush.island.pukhov.output;
 
 public interface Printer {
     void print(Object information);
+
+    void println(Object information);
+
     void printError(RuntimeException exception);
 }
